66fb2562608fb9c51688fce3057a5815405ebcc8
Leonel Peña <leo.dhpl@gmail.com>
2025-12-24 15:56:20 -0300

    feat: added support to soft text

--- components/themed-text.tsx
+++ components/themed-text.tsx
@@ -5,7 +5,7 @@ import { useThemeColor } from '@/hooks/use-theme-color';
 export type ThemedTextProps = TextProps & {
   lightColor?: string;
   darkColor?: string;
-  type?: 'default' | 'title' | 'defaultSemiBold' | 'subtitle' | 'link';
+  type?: 'default' | 'title' | 'defaultSemiBold' | 'subtitle' | 'link' | 'soft';
 };
 
 export function ThemedText({
@@ -26,6 +26,7 @@ export function ThemedText({
         type === 'defaultSemiBold' ? styles.defaultSemiBold : undefined,
         type === 'subtitle' ? styles.subtitle : undefined,
         type === 'link' ? styles.link : undefined,
+        type === 'soft' ? styles.soft : undefined,
         style,
       ]}
       {...rest}
@@ -56,5 +57,10 @@ const styles = StyleSheet.create({
     lineHeight: 30,
     fontSize: 16,
     color: '#0a7ea4',
+  },
+  soft: {
+    fontSize: 16,
+    lineHeight: 24,
+    opacity: 0.6,
   },
 });
