bbac387b59bf94bfa80f398e973f225ecd71c0d9
Leonel Peña <leo.dhpl@gmail.com>
2025-12-25 15:09:22 -0300

    feat: added support to theme in ranking component.

--- components/movie/rating.tsx
+++ components/movie/rating.tsx
@@ -1,3 +1,4 @@
+import { useThemeColor } from '@/hooks/use-theme-color';
 import { StyleSheet } from 'react-native';
 import { Rating as RNRating } from 'react-native-ratings';
 
@@ -9,14 +10,17 @@ type Props = {
  * Displays the movie rating as a 5 stars range.
  */
 export function Rating({ voteAverage }: Props) {
+  const backgroundColor = useThemeColor({}, 'background');
+
   return (
     <RNRating
       type='star'
       ratingCount={5} // shows 5 stars
       imageSize={22}
       readonly // does not allow the user to change the rating
-      startingValue={voteAverage / 2} // voteAverage range is 0 to 10, so we compress it into 0 to 5
+      startingValue={Math.round(voteAverage / 2)} // voteAverage range is 0 to 10, so we compress it into 0 to 5
       style={styles.rating}
+      tintColor={backgroundColor}
     />
   );
 }
